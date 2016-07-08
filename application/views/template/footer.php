fc23381b605d46fb05be53580a52ac43bdfba82e
RedOctober <admin@andy-web-dev.com>
2016-07-08 23:15:34 +1200

    Remove old copyright

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="container">
 	<div class="row">
         <div class="col-md-12">
-		  <p id="footer">Copyright ©<?php echo date('Y'); ?> Icicle Production Company Limited, 7/F, Cheung Wah Industrial Building, 10-12 Shipyard Lane, Quarry Bay. All rights reserved.</p>
+		  <p id="footer">Copyright ©<?php echo date('Y'); ?>. All rights reserved.</p>
         </div>
 	</div>
 </footer>
